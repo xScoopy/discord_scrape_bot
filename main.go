f71862f82078d58b590a16ad7baa4e72f4444b70
xScoopy <69348168+xScoopy@users.noreply.github.com>
2022-03-14 14:59:11 -0500

    set up basic layout and discord bot connection code, no functionality added yet

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	// "encoding/json"
+	// "flag"
+	"fmt"
+	// "io/ioutil"
+	// "net/http"
+	"os"
+	"os/signal"
+
+	// "strings"
+	"log"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+func getEnvVariable(key string) string {
+	//method to retrieve bot token from .env
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Problem loading .env file.")
+	}
+	return os.Getenv(key)
+}
+
+func createMessage() {
+	//func for creating messages from the bot to send to channel
+}
+
+func main() {
+	botToken := getEnvVariable("SECRET")
+    // make new instance with our bot token
+    discordInstance, err := discordgo.New("Bot " + botToken)
+    if err != nil {
+        fmt.Println("error creating Discord session,", err)
+        return
+    }
+
+    // set callback for creating messages
+    discordInstance.AddHandler(createMessage)
+
+    // Bot only used for receiving messages
+    discordInstance.Identify.Intents = discordgo.IntentsGuildMessages
+
+    // connect to discord and listen for events
+    err = discordInstance.Open()
+    if err != nil {
+        fmt.Println("error with opening discord session:,", err)
+        return
+    }
+
+    // wait for kill command
+    fmt.Println("Bot is now running. Press CTRL-C to exit.")
+    sc := make(chan os.Signal, 1)
+    signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+    <-sc
+
+    // Close out session.
+    discordInstance.Close()
+	
+}
